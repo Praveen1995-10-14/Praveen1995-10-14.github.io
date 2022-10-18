1808486f46726631abef016c33c86be54e8f8d50
Praveen <vps8899@gmail.com>
2022-10-18 12:23:03 -0600

    Add .css and .js files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburg = document.querySelector('.menu_for_mbl');
 const overlay = document.querySelector('.open');
 const mainMenu = document.querySelector('.main-menu');
 const cancel = document.querySelector('.cancel');
+const links = document.querySelectorAll('.menu-item');
 
 hamburg.addEventListener('click', () => {
   mainMenu.style.display = 'flex';
@@ -11,3 +12,9 @@ hamburg.addEventListener('click', () => {
 cancel.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
+
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
+}
